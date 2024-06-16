bcd297208c4b6f2d7ae3cab9e84e539a16063ed9
Lu Li <voishion@foxmail.com>
2024-06-16 15:16:00 -0700

    20240616PM001

--- FoodPicker/ContentView.swift
+++ FoodPicker/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    let food = ["汉堡", "沙拉", "披萨", "意大利面", "鸡腿便当", "刀削面", "火锅", "牛肉面", "关东煮"]
+    
+    @State private var selectedFood: String?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing: 30) {
+            Image("dinner")
+                .resizable()
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+            
+            Text("今天吃什么？")
+                .bold()
+            
+            if selectedFood != .none {
+                Text(selectedFood ?? "")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.green)
+            }
+            
+            Button(role: .none) {
+                selectedFood = food.shuffled().first {$0 != selectedFood}
+            } label: {
+                Text(selectedFood == .none ? "请告诉我" : "再换一个").frame(width: 200)
+            }.padding(.bottom, -15)
+            
+            Button(role: .none) {
+                selectedFood = .none
+            } label: {
+                Text("重置").frame(width: 200)
+            }.buttonStyle(.bordered)
         }
         .padding()
+        // 占用最大高度为无限大
+        .frame(maxHeight: .infinity)
+        .background(Color(.secondarySystemBackground))
+        .font(.title)
+        .buttonStyle(.borderedProminent)
+        .buttonBorderShape(.capsule)
+        .controlSize(.large)
+        // 选择食物发生变化时，产生动画
+        .animation(.easeInOut(duration: 1), value: selectedFood)
     }
 }
 
