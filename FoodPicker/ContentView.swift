5ee13489f749ef1f82a212f2e96d744ec3433291
Lu Li <voishion@foxmail.com>
2024-06-16 19:30:36 -0700

    第一章节完成

--- FoodPicker/ContentView.swift
+++ FoodPicker/ContentView.swift
@@ -26,12 +26,23 @@ struct ContentView: View {
                     .font(.largeTitle)
                     .bold()
                     .foregroundColor(.green)
+                    .id(selectedFood) // 解决旧版没有动画的问题，要的就是转场的效果，因为ID发生了改变
+                    // 设定转场效果
+//                    .transition(.scale.combined(with: .slide))
+                    // 分别设定进场、离场的动画效果
+                    .transition(.asymmetric(
+                        insertion: .opacity
+                            .animation(.easeInOut(duration: 0.5).delay(0.2)),
+                        removal: .opacity
+                            .animation(.easeInOut(duration: 0.4))))
             }
             
             Button(role: .none) {
                 selectedFood = food.shuffled().first {$0 != selectedFood}
             } label: {
                 Text(selectedFood == .none ? "请告诉我" : "再换一个").frame(width: 200)
+                    .animation(.none, value: selectedFood) // 不想要动画
+                    .transformEffect(.identity)            // 不想要位置发生变动（不想转场）
             }.padding(.bottom, -15)
             
             Button(role: .none) {
@@ -49,7 +60,7 @@ struct ContentView: View {
         .buttonBorderShape(.capsule)
         .controlSize(.large)
         // 选择食物发生变化时，产生动画
-        .animation(.easeInOut(duration: 1), value: selectedFood)
+        .animation(.easeInOut(duration: 0.6), value: selectedFood)
     }
 }
 
